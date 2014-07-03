ad0312b377666f1f9a416b5946987ef7018d9a63
Romain Tartière <romain@blogreen.org>
2014-07-03 18:55:39 +0200

    Detach from simple_enum.

--- lib/simple_set/set_hash.rb
+++ lib/simple_set/set_hash.rb
@@ -1,26 +1,23 @@
-require 'simple_enum/enum_hash'
-
 module SimpleSet
   class SetHash < ::ActiveSupport::OrderedHash
-    def initialize(args = [], strings = false)
+    def initialize(args = [])
       super()
 
-      if args.is_a?(Hash) then
+      args = if args.is_a?(Hash) then
         args.each { |k,v| set_value_for_reverse_lookup(k, v) }
+      elsif args.is_a?(Array) && !args.first.is_a?(Array) then
+        args.each_with_index.map { |x,y| [x, 2**y] }
       else
-        ary = args.send(args.respond_to?(:enum_with_index) ? :enum_with_index : :each_with_index).map { |x,y| [ x, 2**y ] } unless args.first.respond_to?(:map)
-        ary = args.map { |e| [e, 2**e.id] } if args.first.respond_to?(:map) && !args.first.is_a?(Array)
-        ary ||= args
-        ary.each { |e| set_value_for_reverse_lookup(e[0], strings ? e[0].to_s : e[1]) }
+        raise Exception.new()
       end
+      args.each { |e| set_value_for_reverse_lookup(e[0], e[1]) }
 
       freeze
     end
 
   private
     def set_value_for_reverse_lookup(key, value)
-      sym = ::SimpleEnum::EnumHash.symbolize(key)
-      self[key] = value
+      self[key.to_sym] = value
     end
   end
 end
